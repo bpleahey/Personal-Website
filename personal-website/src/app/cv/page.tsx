1774bbbe010ae5074eb19954e32610f445370788
Brendan <brendan_leahey@brown.edu>
2025-11-20 18:44:56 -0800

    reactive margin sizing for mobile

--- personal-website/src/app/cv/page.tsx
+++ personal-website/src/app/cv/page.tsx
@@ -1,15 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Banner from "@/elements/banner";
-// TODO: make wider?
 
 export default function CV() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  console.log("Current isMobile state:", isMobile);
+
+  const docUrl = isMobile
+    ? "https://docs.google.com/document/d/e/2PACX-1vQZPuN6_rqySVdxqdsfBG3fq1GklewMdjhNTxoZlFoDImp0zFLc3ZMHdFuwbHKFWuinPHhgVq3qHa8X/pub?embedded=true"
+    : "https://docs.google.com/document/d/e/2PACX-1vQdhy-LlszJqunjd575dVnx9QIwRpxPpmckDfXevVY6zWBvaJHg-2rfnz1AHyri4rrF3l_OBYhmlav4/pub?embedded=true";
+
   return (
     <div className={styles.page}>
       <Banner />
-      <iframe
-        className={styles.iframe}
-        src="https://docs.google.com/document/d/e/2PACX-1vQZPuN6_rqySVdxqdsfBG3fq1GklewMdjhNTxoZlFoDImp0zFLc3ZMHdFuwbHKFWuinPHhgVq3qHa8X/pub?embedded=true"
-      ></iframe>
+      <iframe className={styles.iframe} src={docUrl}></iframe>
     </div>
   );
 }
